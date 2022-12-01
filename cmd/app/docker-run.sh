687c90d97dc3ab768e606cc81ac51bea70d74df7
Rômulo Ferreira <romulo.ferreira.silva@g.globo>
2022-12-01 10:07:56 -0300

    fix: relations network

--- cmd/app/docker-run.sh
+++ cmd/app/docker-run.sh
@@ -4,6 +4,15 @@ docker run -d --name db \
               --platform=linux/x86_64 \
               -p 5432:5432 \
               romuloslv/customdb:1.0
+
+docker run -d --name pg \
+              --platform=linux/x86_64 \
+              -e PGADMIN_DEFAULT_EMAIL=admin@admin.com \
+              -e PGADMIN_DEFAULT_PASSWORD=test12321 \
+              -p 80:80 \
+              --link db \
+              dpage/pgadmin4
+
 docker run -d --name api \
               --platform=linux/x86_64 \
               -e APP_POSTGRES_HOST=db \
